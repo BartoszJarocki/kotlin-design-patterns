b8e98dcc499bba0437766ec4b0a86ecc3c0fa867
Bartosz Jarocki <bartosz.jarocki@smartsoft.io>
2017-02-10 09:31:35 +0100

    Added strategy pattern.

---
+++ src/Strategy.kt
@@ -0,0 +1,29 @@
+interface PrintStrategy {
+    fun print(string: String): String
+}
+
+class Printer(val strategy: PrintStrategy) {
+    fun print(string: String): String {
+        return strategy.print(string)
+    }
+}
+
+class UpperCaseStrategy : PrintStrategy {
+    override fun print(string: String): String {
+        return string.toUpperCase()
+    }
+}
+
+class LowerCaseStrategy : PrintStrategy {
+    override fun print(string: String): String {
+        return string.toLowerCase()
+    }
+}
+
+fun main(args: Array<String>) {
+    var lower = Printer(strategy = LowerCaseStrategy())
+    println(lower.print("O tempora, o mores!"))
+
+    var upper = Printer(strategy = UpperCaseStrategy())
+    println(upper.print("O tempora, o mores!"))
+}
