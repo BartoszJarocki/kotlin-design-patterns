71dec2acf2e06a13cb9807a242865ddbf933684c
Bartosz Jarocki <bartosz.jarocki@smartsoft.io>
2017-02-10 18:48:52 +0100

    Code cleanup.

--- src/behavioral/Strategy.kt
+++ src/behavioral/Strategy.kt
@@ -7,25 +7,10 @@ class Printer(val strategy: (String) -> String) {
 val lowerCaseFormatter: (String) -> String = String::toLowerCase
 val upperCaseFormatter: (String) -> String = String::toUpperCase
 
-val addOne = fun(number: Int): Int {
-    return 1 + number
-}
-
-fun test(value: Int, function: (Int) -> Int) {
-    println(function(value))
-}
-
 fun main(args: Array<String>) {
     val lower = Printer(strategy = lowerCaseFormatter)
     println(lower.print("O tempora, o mores!"))
 
     val upper = Printer(strategy = upperCaseFormatter)
     println(upper.print("O tempora, o mores!"))
-
-    test(1, addOne)
-    test(1, { 1 + it })
-
-    test(2) {
-        1 + it
-    }
 }
