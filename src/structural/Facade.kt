0832bb4e127cbcd1575a46ca22b82bcf7d36cdf8
Bartosz Jarocki <bartosz.jarocki@smartsoft.io>
2017-02-13 14:13:23 +0100

    Added facade pattern.

---
+++ src/structural/Facade.kt
@@ -0,0 +1,38 @@
+package structural
+
+class CPU {
+    fun freeze() = println("Freezing.")
+
+    fun jump(position: Long) = println("Jump to $position.")
+
+    fun execute() = println("Executing.")
+}
+
+class HardDrive {
+    fun read(lba: Long, size: Int): ByteArray = byteArrayOf()
+}
+
+class Memory {
+    fun load(position: Long, data: ByteArray) = println("Loading from memory position: $position")
+}
+
+/* Facade */
+class Computer(val processor: CPU = CPU(), val ram: Memory = Memory(), val hd: HardDrive = HardDrive()) {
+    companion object {
+        val BOOT_ADDRESS = 0L
+        val BOOT_SECTOR = 0L
+        val SECTOR_SIZE = 0
+    }
+
+    fun start() {
+        processor.freeze()
+        ram.load(BOOT_ADDRESS, hd.read(BOOT_SECTOR, SECTOR_SIZE))
+        processor.jump(BOOT_ADDRESS)
+        processor.execute()
+    }
+}
+
+fun main(args: Array<String>) {
+    val computer = Computer()
+    computer.start()
+}
